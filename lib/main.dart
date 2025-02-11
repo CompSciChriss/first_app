2ff194547cce59a85c4098be4b4574cfe99e7f19
Christopher Dorin <chrisdorin45@gmail.com>
2025-02-10 23:08:35 -0500

    Finished Gradient Container and Centered Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'gradient_container.dart';
 void main() {
   runApp(
     MaterialApp(
@@ -8,32 +8,4 @@ void main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 12, 1, 39),
-            Color.fromARGB(255, 63, 8, 165),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 34,
-          ),
-        ),
-      ),
-    );
-  }
 }
