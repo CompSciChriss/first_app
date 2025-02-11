e0dfb6535483018c50eb4f1c6b5c1daada686e45
Christopher Dorin <chrisdorin45@gmail.com>
2025-02-10 23:38:06 -0500

    Made Gradient Container Modifiable

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(Alignment.topLeft, Alignment.bottomRight),
       ),
     ),
   );
